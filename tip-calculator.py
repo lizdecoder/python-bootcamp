8ef318dd3c5a1651b108433605f9e9b6cd59ef23
lizdecoder <eys.1908@gmail.com>
2022-10-12 11:11:11 -0400

    uploading tip-calculator.py

---
+++ tip-calculator.py
@@ -0,0 +1,14 @@
+# Python-bootcamp: 100 days of code 
+# Day 2 project
+
+print('Welcome to the tip calculator.')
+bill = float(input('What was the total bill? $'))
+tip_percent = float(input('What percent tip would you like to give? 10, 12, or 15? '))
+people = int(input('How many people to split the bill? '))
+
+tip = tip_percent/100
+payment = (bill / people) * (1.00 + tip)
+
+format_payment = "{:.2f}".format(payment)
+
+print(f'Each person should pay: ${format_payment}')
