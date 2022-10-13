977251dec108ae3e9823139bd0bec8ff43d2b98c
lizdecoder <eys.1908@gmail.com>
2022-10-13 11:33:26 -0400

    Day 3 Project

---
+++ treasure-island.py
@@ -0,0 +1,45 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+direction = input("Would you like to go left or right?").lower()
+if direction == 'left':
+  action = input("Excellent. Would you like to swim through the lake or wait for a bus?").lower()
+  if action == "wait":
+    door = input("You have arrived safely to a building with three doors. Which door would you like to enter through? Red, Yellow, or Blue?").lower()
+    if door == 'blue':
+      print("You have been eating by blood sucking insects! Game Over!")
+    elif door == 'red':
+      print("You have been burned by fire! Game Over!")
+    elif door == 'yellow':
+      print("You are safe and are the sole owner of the treasure! Congratulations, you win the game!")
+    else:
+      print("You have gone missing and will probably die. Game Over!")
+  else:
+    print("You have drown and sharks are eating your flesh! Game Over!")
+else:
+  print("You have fallen off a cliff. Game Over!")
