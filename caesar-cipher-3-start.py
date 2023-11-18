dc6219368885e914bbe8d6ac6159514eb3e55ab6
lizdecoder <eys.1908@gmail.com>
2023-11-18 09:08:54 -0500

    One function for encoding and decoding.

---
+++ caesar-cipher-3-start.py
@@ -0,0 +1,51 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+#TODO-1: Combine the encrypt() and decrypt() functions into a single function called caesar(). 
+# first attempt
+def caesar(start_text, shift_amount, cipher_direction):
+    end_text = ""
+    # better solve to incorrect shift is to move if statement outside of for loop
+    if cipher_direction == 'decode':
+        shift_amount *= -1
+    for letter in start_text:
+        position = alphabet.index(letter)
+        # if cipher_direction == 'encode':
+        #    new_position = position + shift_amount
+        # elif cipher_direction == 'decode':
+        #    new_position = position - shift_amount
+        # second attempt: only decode shift_amount multipled by -1
+        # if cipher_direction == 'decode':
+            # fixing bug of -5, 5, -5, 5 attempt one
+            # shift_amount = -abs(shift_amount)
+            # print(f"shift if: {shift_amount}")
+        new_position = position + shift_amount
+        end_text += alphabet[new_position]
+    print(f"The {cipher_direction}d text is {end_text}")
+
+# def encrypt(plain_text, shift_amount):
+#   cipher_text = ""
+#   for letter in plain_text:
+#     position = alphabet.index(letter)
+#     new_position = position + shift_amount
+#     cipher_text += alphabet[new_position]
+#   print(f"The encoded text is {cipher_text}")
+
+# def decrypt(cipher_text, shift_amount):
+#   plain_text = ""
+#   for letter in cipher_text:
+#     position = alphabet.index(letter)
+#     new_position = position - shift_amount
+#     plain_text += alphabet[new_position]
+#   print(f"The decoded text is {plain_text}")
+
+# if direction == "encode":
+#   encrypt(plain_text=text, shift_amount=shift)
+# elif direction == "decode":
+#   decrypt(cipher_text=text, shift_amount=shift)
+
+#TODO-2: Call the caesar() function, passing over the 'text', 'shift' and 'direction' values.
+caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
