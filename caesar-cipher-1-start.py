482234cfe484adbcb77ea25f94ccf8a42952ae57
lizdecoder <eys.1908@gmail.com>
2023-11-18 07:59:12 -0500

    debugging out of bounds letter error

--- caesar-cipher-1-start.py
+++ caesar-cipher-1-start.py
@@ -1,4 +1,5 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+# option #2: double list to solve out of bounds letters issue
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
@@ -6,13 +7,29 @@ shift = int(input("Type the shift number:\n"))
 
 #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 
-def encrypt(text, shift):
-    new_text = []
-    for letter in list(text):
-        new_letter = alphabet[alphabet.index(letter)+shift]
-        new_text.append(new_letter)
-    print(f'The encoded text is', "".join(new_text))
+# first attempt
+# def encrypt(text, shift):
+#     new_text = []
+#     for letter in list(text):
+#         new_letter = alphabet[alphabet.index(letter)+shift]
+#         new_text.append(new_letter)
+#     print(f'The encoded text is', "".join(new_text))
 
+# solution
+# always use different names for the parameters
+def encrypt(plain_text, shift_amount):
+    cipher_text = ""
+    for letter in plain_text:
+        position = alphabet.index(letter)
+        new_position = position + shift_amount
+        # adding a safeguard for letters that fall out of range: 1st attempt
+        # one option to solve issue with out of bounds letters
+        # if new_position >= len(alphabet)-1:
+        #     new_index = new_position - (len(alphabet))
+        #     new_position = new_index
+        new_letter = alphabet[new_position]
+        cipher_text += new_letter
+    print(f"The encoded text is {cipher_text}")
 
     #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
     #e.g. 
@@ -27,4 +44,4 @@ def encrypt(text, shift):
     ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
 
 #TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
-encrypt(text, shift)
+encrypt(plain_text=text, shift_amount=shift)
