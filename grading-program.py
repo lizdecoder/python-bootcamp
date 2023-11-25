2c19ddac17dc5fdd4edba6ca15c4aea31936ad5b
lizdecoder <eys.1908@gmail.com>
2023-11-25 18:45:25 -0500

    day 9 challenge 2

--- grading-program.py
+++ grading-program.py
@@ -10,15 +10,27 @@ student_scores = {
 student_grades = {}
 
 # TODO-2: Write your code below to add the grades to student_grades.👇
-for key in student_scores:
-    if student_scores[key] >= 91:
-        student_grades[key] = "Outstanding"
-    elif student_scores[key] >= 81:
-        student_grades[key] = "Exceeds Expectations"
-    elif student_scores[key] >= 71:
-        student_grades[key] = "Acceptable"
+# first attempt
+# for key in student_scores:
+#     if student_scores[key] >= 91:
+#         student_grades[key] = "Outstanding"
+#     elif student_scores[key] >= 81:
+#         student_grades[key] = "Exceeds Expectations"
+#     elif student_scores[key] >= 71:
+#         student_grades[key] = "Acceptable"
+#     else:
+#         student_grades[key] = "Fail"
+for student in student_scores:
+    score = student_scores[student]
+    if student_scores[student] > 90:
+        student_grades[student] = "Outstanding"
+    elif student_scores[student] > 80:
+        student_grades[student] = "Exceeds Expectations"
+    elif student_scores[student] > 70:
+        student_grades[student] = "Acceptable"
     else:
-        student_grades[key] = "Fail"
+        student_grades[student] = "Fail"
+
 
 # 🚨 Don't change the code below 👇
 print(student_grades)
