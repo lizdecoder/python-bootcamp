ec9bb4f4ab759226424d625504c61a37eca3d699
lizdecoder <eys.1908@gmail.com>
2023-11-26 07:48:45 -0500

    adding docstring

--- day-10-start.py
+++ day-10-start.py
@@ -1,6 +1,8 @@
 #Functions with Outputs
 
 def format_name(f_name, l_name):
+    # docstrings to define functions
+    '''Take a first and last name and format it to return the title case version of the  name.'''
     return f"{f_name.title()} {l_name.title()}"
 
 print(format_name("liz", "skert"))
