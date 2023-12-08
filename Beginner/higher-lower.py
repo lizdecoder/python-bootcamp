13f56c25e96588da8186404b0cc9cc21c18787b2
lizdecoder <eys.1908@gmail.com>
2023-12-08 17:27:46 -0500

    higher-lower-solution and notes

--- Beginner/higher-lower.py
+++ Beginner/higher-lower.py
@@ -1,3 +1,10 @@
+# my attempt to the higher-lower game
+## learns after reviewing solution:
+## use function for formatting account data
+## logo gets reprinted after clear
+## only need one while loop to make game repeatedable vs having a function called game
+
+
 from random import randint
 import os
 from higher_lower_art import logo, vs
@@ -13,47 +20,43 @@ def highest_follower_count(selection_A_data, selection_B_data):
     elif selection_A_data['follower_count'] < selection_B_data['follower_count']:
         return selection_B_data['name']
 
-def game():
-    score = 0        
-    selection_A = make_random_selection()
-    selection_B = make_random_selection()
-    game_not_finished = True
+# def game():
+# logo is printed twice
+print(logo)
+score = 0        
+selection_A = make_random_selection()
+selection_B = make_random_selection()
+game_not_finished = True
     
-    while game_not_finished:
-        # TODO: must determine proper location of logo
-        print(logo)
-        print(f"Compare A: {selection_A['name']}, {selection_A['description']}, {selection_A['country']}")
-        print(f"***TESTING: follower = {selection_A['follower_count']}")
-        print(vs)
-        print(f"Against B: {selection_B['name']}, {selection_B['description']}, {selection_B['country']}")
-        print(f"***TESTING: follower = {selection_B['follower_count']}")
+while game_not_finished:
+    print(f"Compare A: {selection_A['name']}, {selection_A['description']}, {selection_A['country']}")
+    print(vs)
+    # Should have made a function instead of repeating data
+    print(f"Against B: {selection_B['name']}, {selection_B['description']}, {selection_B['country']}")
+    print(f"***TESTING: follower = {selection_B['follower_count']}")
 
-        guess = input("Who has more followers? Type 'A' or 'B': ").upper()
-        if guess == 'A':
-            guess = selection_A['name']
-        elif guess == 'B':
-            guess = selection_B['name']
+    guess = input("Who has more followers? Type 'A' or 'B': ").upper()
+    if guess == 'A':
+        guess = selection_A['name']
+    elif guess == 'B':
+        guess = selection_B['name']
 
-        answer = highest_follower_count(selection_A, selection_B)
-        print(f"***TESTING guess is: {guess} and answer is: {answer}")
-        # TODO: need to make code more efficient, without clear, there's double information
-        # os.system('clear')
+    answer = highest_follower_count(selection_A, selection_B)
+    # clear is needed here and reprint of loogo
+    os.system('clear')
+    print(logo)
 
-        if guess == answer:
-            score += 1
-            # TODO: must determine proper location of logo; duplicate now
-            # print(logo)
-            print(f"You're right! Current score: {score}")
-        else:
-            # This works as intended
-            game_not_finished = False
-            # TODO: must determine proper location of logo; this is technically right, but it's due to clear screen
-            # print(logo)
-            print(f"Sorry, that's wrong. Final score: {score}")
-        
-        selection_A = selection_B
-        selection_B = make_random_selection()
+    if guess == answer:
+        score += 1
+        print(f"You're right! Current score: {score}")
+    else:
+        # This works as intended
+        game_not_finished = False
+        print(f"Sorry, that's wrong. Final score: {score}")
+    
+    selection_A = selection_B
+    selection_B = make_random_selection()
 
-game()
+# game()
 
 
