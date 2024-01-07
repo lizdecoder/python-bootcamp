8758a8b3052d05c771d927d73609dd62d8d0e309
lizdecoder <eys.1908@gmail.com>
2024-01-06 22:22:59 -0500

    Day 19 projects starts

--- Intermediate/day-19/main.py
+++ Intermediate/day-19/main.py
@@ -8,6 +8,35 @@ def move_forwards():
     tim.forward(10)
 
 
+def move_backwards():
+    tim.backward(10)
+
+
+def move_counterclockwise():
+    # tim.left(10)
+    new_heading = tim.heading() + 10
+    tim.setheading(new_heading)
+
+
+def move_clockwise():
+    # tim.right(10)
+    new_heading = tim.heading() - 10
+    tim.setheading(new_heading)
+
+
+def clear_drawing():
+    # clear drawing and put turtle back at center
+    # screen.resetscreen()
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+
+
 screen.listen()
-screen.onkey(key="space", fun=move_forwards)
+screen.onkey(key="w", fun=move_forwards)
+screen.onkey(key="s", fun=move_backwards)
+screen.onkey(key="a", fun=move_counterclockwise)
+screen.onkey(key="d", fun=move_clockwise)
+screen.onkey(key="c", fun=clear_drawing)
 screen.exitonclick()
---
+++ Intermediate/day-19/turtle_race.py
@@ -0,0 +1,8 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+screen.setup(width=500, height=400)
+
+
+screen.exitonclick()
